d62a63cd7df196593c8ea2f12fd92338a4fbe6e3
Shaun Henderson <shaun@Shauns-MBP.attlocal.net>
2023-01-19 13:31:42 -0500

    Connected User Model, created user

--- db/init_db.js
+++ db/init_db.js
@@ -1,5 +1,6 @@
 const {
   client,
+  User
   // declare your model imports here
   // for example, User
 } = require('./');
@@ -7,10 +8,26 @@ const {
 async function buildTables() {
   try {
     client.connect();
+    console.log("Starting to drop tables")
 
-    // drop tables in correct order
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `)
 
-    // build tables in correct order
+    console.log("Finished dropping tables");
+
+    console.log("Starting to create tables");
+
+    await client.query(`
+      CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        "isAdmin" BOOLEAN DEFAULT false
+      );
+    `)
+
+    console.log("Finished creating tables");
   } catch (error) {
     throw error;
   }
@@ -18,6 +35,8 @@ async function buildTables() {
 
 async function populateInitialData() {
   try {
+    const user1 = await User.createUser({username: "Deadsent", password: "Deadman2", isAdmin: true})
+    console.log("Here is user1", user1)
     // create useful starting data by leveraging your
     // Model.method() adapters to seed your db, for example:
     // const user1 = await User.createUser({ ...user info goes here... })
--- db/models/user.js
+++ db/models/user.js
@@ -4,8 +4,26 @@ const client = require('../client');
 module.exports = {
   // add your database adapter fns here
   getAllUsers,
+  createUser
 };
 
 async function getAllUsers() {
   /* this adapter should fetch a list of users from your db */
+}
+
+async function createUser({username, password, isAdmin}) {
+  try {
+   const {rows: [user]} = await client.query(`
+      INSERT INTO users(username, password, "isAdmin")
+      VALUES($1, $2, $3)
+      ON CONFLICT (username) DO NOTHING
+      RETURNING *;
+    `, [username, password, isAdmin])
+
+    console.log(user)
+    return user
+  } catch (error) {
+    console.error("Error Creating A User", error)
+    throw error
+  }
 }
